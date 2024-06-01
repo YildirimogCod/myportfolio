7953d6a4d9c3568a67e667210e5dfc9e44f6ca14
Oğuzhan Yıldırım <yildirimogcod@gmail.com>
2024-06-01 20:41:07 +0300

    color changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
               variants={container(0.5)}
               initial="hidden"
               animate="visible"
-              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
+              className="bg-gradient-to-r from-pink-500 via-slate-600 to-purple-700 bg-clip-text text-4xl tracking-tight text-transparent"
             >
               Front-end Developer
             </motion.span>
